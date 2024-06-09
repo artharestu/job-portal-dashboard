0975423f79fb293eaf52984754d49ce74e7d89b2
artharestu <artharestuabadi@gmail.com>
2024-06-09 20:52:58 +0700

    Post Job Page

---
+++ src/app/post-a-job/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface postJobPageProps {}
+
+const postJobPage: FC<postJobPageProps> = ({}) => {
+  return <div>postJobPage</div>;
+};
+
+export default postJobPage;
