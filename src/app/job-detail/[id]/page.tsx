cfc01d72e544be01a8482d0710e75a28f38764a7
artharestu <artharestuabadi@gmail.com>
2024-06-13 15:38:18 +0700

    Tab Applicant

--- src/app/job-detail/[id]/page.tsx
+++ src/app/job-detail/[id]/page.tsx
@@ -1,9 +1,40 @@
+import Applicants from "@/components/organisms/Applicants";
+import JobDetails from "@/components/organisms/JobDetails";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ArrowLeftIcon } from "lucide-react";
+import Link from "next/link";
 import React, { FC } from "react";
 
 interface JobDetailPageProps {}
 
 const JobDetailPage: FC<JobDetailPageProps> = ({}) => {
-  return <div>JobDetailPage</div>;
+  return (
+    <>
+      <div className="inline-flex items-center gap-5 mb-5">
+        <div>
+          <Link href="/job-listings">
+            <ArrowLeftIcon className="w-9 h-9" />
+          </Link>
+        </div>
+        <div>
+          <div className="text-2xl font-semibold mb-1">Brand Designer</div>
+          <div>Design . Full-time . 1/10 Hired</div>
+        </div>
+      </div>
+      <Tabs defaultValue="applicants">
+        <TabsList className="mb-8">
+          <TabsTrigger value="applicants">Applicants</TabsTrigger>
+          <TabsTrigger value="jobdetails">Job Details</TabsTrigger>
+        </TabsList>
+        <TabsContent value="applicants">
+          <Applicants />
+        </TabsContent>
+        <TabsContent value="jobdetails">
+          <JobDetails />
+        </TabsContent>
+      </Tabs>
+    </>
+  );
 };
 
 export default JobDetailPage;
---
+++ src/components/organisms/JobDetails/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface JobDetailsProps {}
+
+const JobDetails: FC<JobDetailsProps> = ({}) => {
+  return <div>JobDetails</div>;
+};
+
+export default JobDetails;
