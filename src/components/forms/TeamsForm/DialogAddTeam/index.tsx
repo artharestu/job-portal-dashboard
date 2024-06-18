351ac9b87548e80fc3a96606bf85db8b1cb0a1c4
artharestu <artharestuabadi@gmail.com>
2024-06-18 19:52:58 +0700

    Teams Form

---
+++ src/components/forms/TeamsForm/DialogAddTeam/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface DialogAddTeamProps {}
+
+const DialogAddTeam: FC<DialogAddTeamProps> = ({}) => {
+  return <div>DialogAddTeam</div>;
+};
+
+export default DialogAddTeam;
